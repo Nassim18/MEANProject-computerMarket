b050550b513acdad7c92e720db17e4283bc4f294
SHOUT Oussama Nassim <56071445+Nassim18@users.noreply.github.com>
2020-09-20 15:21:39 +0200

    Delete creationbase.sh

--- INFODB/Serveur/creationbase.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --db INFODB --collection membres --file membres.json --jsonArray --drop
-mongoimport --db INFODB --collection produits --file produits.json --jsonArray --drop
-mongoimport --db INFODB --collection panier --file panier.json --jsonArray --drop
